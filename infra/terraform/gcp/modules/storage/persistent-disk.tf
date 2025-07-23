2baaf8de7d5df86e83410252c32ad46da9c8260b
git-ig <ivan.pavlovych@gmail.com>
2025-07-23 17:03:01 +0000

    Update terraform and workflows

---
+++ infra/terraform/gcp/modules/storage/persistent-disk.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_storage_class_v1" "database" {
+  metadata {
+    name = "database-storage"
+  }
+
+  storage_provisioner    = "pd.csi.storage.gke.io"
+  reclaim_policy        = "Retain"
+  volume_binding_mode   = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+
+  parameters = {
+    type = "pd-balanced"
+  }
+}
+
+output "database_storage_class" {
+  description = "Name of the database storage class"
+  value       = kubernetes_storage_class_v1.database.metadata[0].name
+}
